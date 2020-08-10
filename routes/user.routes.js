33b9439c50cae3d279485fb6ffc539f7a94eb0e8
Bodei Brouwer <bodeibrouwer@gmail.com>
2020-08-10 17:10:24 +0200

    country list added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const router  = express.Router();
 const bcryptjs = require('bcryptjs');
 const BookModel = require('../models/Books.model');
 const MovieModel = require('../models/Movies.model')
+const CountryModel = require('../models/Country.model')
 
 router.get('/map', (req, res, next) => {
   if (req.session.loggedInUser){
@@ -15,12 +16,22 @@ router.get('/map', (req, res, next) => {
 
 router.get('/new-country', (req, res, next) => {
   if (req.session.loggedInUser){
-    res.render('users/create-new.hbs', {loggedInUser: req.session.loggedInUser})
+    CountryModel.find()
+     .then((countries) => {
+      res.render('users/create-new.hbs', {countries})
+     })
   }
   else {
     res.redirect('/signin')
   }
 });
+
+router.get('/:id', (req, res) => {
+  CountryModel.findById(req.params.id)
+      .then((country) => {
+          res.render('users/country-details.hbs', {country})
+      })
+})
 
 router.get('/profile', (req, res) => {
   if (req.session.loggedInUser){
