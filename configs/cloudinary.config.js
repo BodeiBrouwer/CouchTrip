9dc2d688214f88d0be54fc366c204c5e2677fdb8
Britta Helm <brittahelm@gmx.net>
2020-08-13 11:44:38 +0200

    add route to navbar mytrips

---
+++ configs/cloudinary.config.js
@@ -0,0 +1,23 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+require('dotenv').config();
+
+cloudinary.config({
+  cloud_name: 'deravtpot',
+  api_key: '${process.env.CLOUDINARYKEY}',
+  api_secret: '${process.env.CLOUDINARYSECRET}'
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  folder: 'couchtrip-gallery', // The name of the folder in cloudinary
+  allowedFormats: ['jpg', 'png'],
+  // params: { resource_type: 'raw' }, => this is in case you want to upload other type of files, not just images
+  filename: function (req, res, cb) {
+    cb(null, res.originalname); // The file on cloudinary would have the same name as the original file name
+  }
+});
+
+module.exports = multer({ storage });
