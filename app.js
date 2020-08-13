5ecdc9f2bcf4a8ca6b65f9994e08a6446b3b7d66
Bodei Brouwer <bodeibrouwer@gmail.com>
2020-08-13 14:09:37 +0200

    profile edit fixes

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-const MONDOGB_URL = process.env.MONGODB_URI || 'mongodb://localhost/couchtrip'
+const MONDOGB_URI = process.env.MONGODB_URI || 'mongodb://localhost/couchtrip'
 
 mongoose
-  .connect(`${MONDOGB_URL}`, {useNewUrlParser: true})
+  .connect(`${MONDOGB_URI}`, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
